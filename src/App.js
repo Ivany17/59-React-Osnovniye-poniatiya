bc3b19a030e3a3b36bb8bd3e8b7c702b1ec87004
Ivan Yaremko <yaremich98@gmail.com>
2023-03-14 10:29:16 +0200

    (59) React. Основные понятия

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Ciao from './components/Ciao';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const user = {
+//     firstName: 'Ivan',
+//     lastName: 'Yarem'
+//   }
+//   return <React.Fragment>
+//     <Ciao name={`${user.firstName} ${user.lastName}`} />
+//     <Ciao name='Nazar Ptushch' />
+//     <Ciao name='Ann Lezed' />
+//   </React.Fragment>
+
+// }
+
+// export default App;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isStraight:true,
+      users: [
+        {
+          id:1,
+          firstName:'Elon'
+        },
+        {
+          id:2,
+          firstName:'Nazar'
+        },
+        {
+          id:3,
+          firstName:'Ann'
+        }
+      ]
+    }
+    sortUsers = () => {
+      const {users, isStraight} = this.setState;
+      const sortUsers = JSON.parse(JSON.stringify(users));
+      //const sortUsers = [...users];
+      sortUsers.sort((prev, next) => isStraight ? next.id - prev.id : prev.id - next.id);
+      this.setState({
+        isStraight:isStraight,
+        users:sortUsers
+      })
+    }
+  }
+  render(){
+    const {users, isStraight} = this.state;
+    return <>
+    <p>
+    <button onClick={this.sortUsers}>SORT BY NUMBER{isStraight ? 'straight' : 'reverse'}</button>
+    {/* <button onClick={this.sortUsers}>SORT BY NAME{isAlphabet ? 'straight' : 'reverse'}</button> */}
+    </p>
+      <div>
+        {users.map(({firstName, id}) =><Ciao key={id} id={id} name={firstName} />)}
+      </div>
+    </>
+
+  }
 }
 
 export default App;
